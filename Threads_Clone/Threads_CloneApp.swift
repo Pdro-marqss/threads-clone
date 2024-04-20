75eb4704d3d6ec21659199fae13bde171a930071
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-19 21:08:20 -0300

    Initial Commit

---
+++ Threads_Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads_Clone
+//
+//  Created by Pedro Marques on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

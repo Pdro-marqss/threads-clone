691551d13c398d493b77dc42b03a7a0611d63ed5
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-19 21:16:35 -0300

    Estrutura inicial do projeto

---
+++ Threads_Clone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Threads_Clone
+//
+//  Created by Pedro Marques on 19/04/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
